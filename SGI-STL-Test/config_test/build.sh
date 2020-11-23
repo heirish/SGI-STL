f0f865a92c882a437620d26dbb5f4a1ee3add6d6
lihaiyan <haiyan.li@bst.ai>
2020-11-23 02:21:47 +0000

    add config-test

--- SGI-STL-Test/config_test/build.sh
+++ SGI-STL-Test/config_test/build.sh
@@ -1,6 +1,7 @@
-rm -rf build 2>&1 >null
+#!/bin/bash
+
+rm -rf build >/dev/null 2>&1
 mkdir build
 cd build
-cmake clean
-cmake -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" ..
-make  -j 4
+cmake -DCMAKE_C_COMPILER:FILEPATH=D:\\cygwin64\\bin\\gcc.exe -DCMAKE_CXX_COMPILER:FILEPATH=D:\\cygwin64\\bin\\g++.exe -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" ..
+make -j 4
--- SGI-STL-Test/config_test/config_test.cpp
+++ SGI-STL-Test/config_test/config_test.cpp
@@ -11,5 +11,15 @@ int main(int argc, char* argv[]) {
     std::cout << __GNUC__ << " " << __GNUC_MINOR__ << std::endl;
     #endif
 
-    
+    #ifdef __STL_NO_DRAND48
+    std::cout << "__STL_NO_DRAND48 defined" <<std::endl;
+    #else
+    std::cout << "__STL_NO_DRAND48 undefined" << std::endl;
+    #endif
+
+    #ifdef __STL_STATIC_TEMPLATE_MEMBER_BUG
+    std::cout << "__STL_STATIC_TEMPLATE_MEMBER_BUG defined" << std::endl;
+    #else
+    std::cout << "__STL_STATIC_TEMPLATE_MEMBER_BUG undefined" << std::endl;
+    #endif
 }
